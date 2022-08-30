bbe5e8e9e66ffc8a9816ce1422b85ef059f6f87b
aalred <aalex.red1@gmail.com>
2022-08-30 12:28:15 -0500

    fix(src/models/reviews.js): fixed model

--- src/models/reviews.js
+++ src/models/reviews.js
@@ -2,8 +2,9 @@ const { DataTypes } = require('sequelize');
 module.exports = (sequelize) => {
   sequelize.define('reviews', {
     id: {
-      type: DataTypes.UUIDV1,
-      defaultValue: DataTypes.UUIDV4
+      type: DataTypes.INTEGER,
+      defaultValue: DataTypes.INTEGER,
+      primaryKey: true,
     },
     rating: {
       type: DataTypes.NUMBER,

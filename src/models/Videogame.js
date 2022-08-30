44784a752cfd4a98e3a1704d2063be530a4ca280
aalred <aalex.red1@gmail.com>
2022-08-29 22:03:49 -0500

    feat(src/genre.js): added model genre

--- src/models/Videogame.js
+++ src/models/Videogame.js
@@ -1,12 +1,17 @@
 const { DataTypes } = require('sequelize');
-// Exportamos una funcion que define el modelo
-// Luego le injectamos la conexion a sequelize.
 module.exports = (sequelize) => {
-  // defino el modelo
-  sequelize.define('videogame', {
+  sequelize.define('genre', {
     name: {
       type: DataTypes.STRING,
       allowNull: false,
     },
+
+    id: {
+      type: DataTypes.UUIDV1,
+      defaultValue: DataTypes.UUIDV4
+    },
+    imageBackground:{
+      type: DataTypes.STRING,
+    }
   });
 };

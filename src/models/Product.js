f0fa853dfa35ba8c38127e76a837a3fe93747804
facucorona <coronafacundo@gmail.com>
2022-08-29 23:57:41 -0300

    Model/Product

--- src/models/Product.js
+++ src/models/Product.js
@@ -4,9 +4,86 @@ const { DataTypes } = require('sequelize');
 module.exports = (sequelize) => {
   // defino el modelo
   sequelize.define('products', {
+    id: {
+      type: DataTypes.UUID,
+      defaultValue: DataTypes.UUIDV4,
+      primaryKey: true,
+    },
     name: {
       type: DataTypes.STRING,
       allowNull: false,
     },
+    description: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    rating: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    background_image: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    released: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    screenshots: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    trailer: {
+      type: DataTypes.STRING,      
+    },
+    requeriments_recomended: {
+      type: DataTypes.STRING,      
+    },
+    requeriments_min: {
+      type: DataTypes.STRING,      
+    }, 
+    price: {
+      type: DataTypes.STRING,      
+    }, 
+    isDisabled: {
+      type: DataTypes.BOOLEAN,
+      allowNull: false,
+    },
+    onSale: {
+      type: DataTypes.BOOLEAN,
+      allowNull: false,
+    },
+    esrb_rating: {
+      type: DataTypes.INTEGER,
+      // allowNull: false,
+    },
+    genre_id: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    esrb_rating: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+    },    
+    
   });
 };
+
+
+// id
+// // name
+// description
+// rating
+// backgroun_image
+// released
+// screenshots
+// trailer
+// req.recomended
+// prices
+// req min
+
+// isDisabled
+// onSale
+// esrb_rating
+// genre_id
+// esrb_rating_id

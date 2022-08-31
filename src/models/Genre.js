2a9f22b98464fa8a0f871107be77221186927a08
linker <diegolucas142@gmail.com>
2022-08-31 16:19:39 -0300

    Feature/Route-GetGenre

--- src/models/Genre.js
+++ src/models/Genre.js
@@ -1,17 +1,8 @@
 const { DataTypes } = require('sequelize');
 module.exports = (sequelize) => {
   sequelize.define('Genre', {
-    id: {
-      type: DataTypes.INTEGER,
-      defaultValue: DataTypes.INTEGER,
-      primaryKey: true,
-    },
     name: {
       type: DataTypes.STRING,
-      allowNull: false,
     },
-    imageBackground:{
-      type: DataTypes.STRING,
-    }
-  });
+  }, {timestamps: false});
 };

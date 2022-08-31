9047062a8edaa7c51f1293ba1c739d40336e6c9d
aalred <aalex.red1@gmail.com>
2022-08-31 18:31:19 -0500

    added files to login

---
+++ src/controllers/loginUser.js
@@ -0,0 +1,11 @@
+const { Router } = require('express');
+const router = Router();
+
+router.get('/', (req, res)=>{
+    res.send('ok')
+})
+
+router.get('/', (req, res)=>{
+    res.send('ok')
+})
+module.exports = router;

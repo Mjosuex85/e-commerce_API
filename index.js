ff9ff8ca390b79f96ad5f64cb311c89927edc0e6
aalred <aalex.red1@gmail.com>
2022-09-01 16:15:42 -0500

    updated files

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
 const server = require('./src/app.js');
 const { conn } = require('./src/db.js');
 
-conn.sync({ force: false }).then(() => {
-  server.listen(3001, () => {
-    console.log('%s listening at 3001'); // eslint-disable-line no-console
+// Syncing all the models at once.
+conn.sync({ force: true }).then(() => {
+  server.listen(process.env.PORT, () => {
+    console.log('%s listening at ' + process.env.PORT); // eslint-disable-line no-console
   });
 });
--- src/app.js
+++ src/app.js
@@ -14,13 +14,11 @@ const routes = require('./routes/index.js');
 server.name = 'API';
 
 const {KEY_SECRET}= process.env;
-
-server.use(bodyParser.urlencoded({ extended: true, limit: '50mb' }));
 server.use(bodyParser.json({ limit: '50mb' }));
 server.use(cookieParser(KEY_SECRET));
 server.use(morgan('dev'));
 server.use((req, res, next) => {
-  res.header('Access-Control-Allow-Origin', 'http://localhost:3000'); // update to match the domain you will make the request from
+  res.header('Access-Control-Allow-Origin', '*'); // update to match the domain you will make the request from
   res.header('Access-Control-Allow-Credentials', 'true');
   res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
   res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE');

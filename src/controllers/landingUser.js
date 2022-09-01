188b8a25f96f492a376ea2650851a2da9d37c532
aalred <aalex.red1@gmail.com>
2022-09-01 13:56:09 -0500

    added routes user for buy & add favorites

--- src/controllers/landingUser.js
+++ src/controllers/landingUser.js
@@ -1,19 +1,46 @@
-// Access for all the user info
-
 const { Router } = require('express');
+const jwt = require('jsonwebtoken');
+
+const { Users } = require('../db');
+
 const router = Router();
 
-const jwt = require('jsonwebtoken')
-
 function isAuthenticaded(req, res, next){
-    if(req.isAuthenticaded) return next()
+    if(req.isAuthenticated) return next()
     res.redirect('/login')
 }
 
-router.get('/', isAuthenticaded, (req, res)=>{
-    // User Orders
-    // User Favorites
-    // create JWT
-    res.send('Your enter')
+router.get('/', isAuthenticaded, async(req, res)=>{
+    try {      
+        const {id} = req.user.dataValues;
+        const userAuth = await Users.findOne({ where: {id}, include:'Products'})
+        res.json({message: 'Welcome '+ userAuth.name, user: userAuth})
+    } catch (error) {
+        res.status(404).json({error: error.message})
+    }
+})
+
+router.get('/addFavorite', isAuthenticaded, async(req, res)=>{
+    try {
+        const {id} = req.user.dataValues;
+        const { productId }= req.query;
+        const userAuth = await Users.findByPk(id);
+        userAuth.addProducts(productId, {through: 'Favorites'})
+        res.send('Added to Favorites')
+    } catch (error) {
+        res.status(404).json({error: error.message});
+    }
+})
+
+router.get('/buy', isAuthenticaded, async(req, res)=>{
+    try {
+        const {id} = req.user.dataValues;
+        const { productId }= req.query;
+        const userAuth = await Users.findByPk(id);
+        userAuth.addProducts(productId, {through: 'Order'})
+        res.send('Buy succesfully')
+    } catch (error) {
+        
+    }
 })
 module.exports = router;

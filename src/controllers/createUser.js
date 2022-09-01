567f0ad3752cb38db826da4c5e6738a39d329835
aalred <aalex.red1@gmail.com>
2022-09-01 18:21:38 -0500

    updated file

--- src/controllers/createUser.js
+++ src/controllers/createUser.js
@@ -1,5 +1,5 @@
-const Router = require('Express')
-const router = Router()
+const Router = require('Express');
+const router = Router();
 
 const bcrypt = require('bcrypt');
 
@@ -16,19 +16,19 @@ router.post('/', async (req, res )=>{
         const validateUser = await validateUserRegister(username, name, lastname, email, password);
         if (validateUser) {
             bcrypt.hash(password, keySalt, async function (err, hash) {
-                if(err) throw new Error('something is wrong with the password') 
-                password = hash
-                await Users.create({username, name, lastname, email, password, profile_pic})
+                if(err) throw new Error('something is wrong with the password'); 
+                password = hash;
+                await Users.create({username, name, lastname, email, password, profile_pic});
             })
         }
-        res.send('Register Complete')
+        res.send('Register Complete');
     } catch (error) {
-        res.status(404).send(error.message)
+        res.status(404).send(error.message);
     }
 })
 
 router.get('/', (req, res) =>{
-    res.json({"message":'send a post'})
+    res.json({"message":'send a post'});
 })
 
 module.exports= router;

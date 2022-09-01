4615152a6208ab6a98339f9726456c7d9c9e427a
aalred <aalex.red1@gmail.com>
2022-09-01 00:26:22 -0500

    created routes for login & user info

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,17 +1,21 @@
 const { Router } = require('express');
+
 const conVideogames = require ("../controllers/conVideogames")
 const conGenres = require ("../controllers/conGenres")
 const conPlatforms = require ("../controllers/conPlatforms")
 const loginUser = require("../controllers/loginUser")
-const landing = require("../controllers/landing")
+const logout = require("../controllers/logout")
+const createUser = require('../controllers/createUser')
+const landingUser = require('../controllers/landingUser')
 
 const router = Router();
 
-router.use("/", landing);
 router.use("/videogames", conVideogames);
 router.use("/genres", conGenres);
 router.use("/platforms", conPlatforms);
 router.use("/login", loginUser);
-router.use("/signup", conVideogames);
+router.use("/logout", logout);
+router.use("/signup", createUser);
+router.use("/user", landingUser);
 
 module.exports = router;

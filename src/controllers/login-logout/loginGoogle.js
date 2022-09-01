d1c5e6c119d269b63178225fbd3fcd6863536194
aalred <aalex.red1@gmail.com>
2022-09-01 18:18:21 -0500

    create folder & remove files

---
+++ src/controllers/login-logout/loginGoogle.js
@@ -0,0 +1,46 @@
+const { Router } = require('express');
+const passport = require('passport');
+const bcrypt = require('bcrypt');
+var GoogleStrategy = require('passport-google-oidc');
+
+const { Users } = require('../../db');
+
+const { GOOGLE_CLIENT_ID, GOOGLE_CLIENT_SECRET, URL} = process.env;
+
+
+const router = Router();
+
+passport.use(new GoogleStrategy({
+    clientID: GOOGLE_CLIENT_ID,
+    clientSecret: GOOGLE_CLIENT_SECRET,
+    callbackURL: `http://${URL}/auth/google/redirect`,
+    passReqToCallback: true,
+}, function(request, accessToken, refreshToken, profile, done) {
+    return done(null, profile);
+}));
+
+passport.serializeUser((user, done) =>{
+    done(null, user.id)
+});
+
+passport.deserializeUser(async (id, done) => {
+    const user = await Users.findByPk(id) 
+    done(null, user)
+});
+
+router.get( '/google/redirect',
+  passport.authenticate( 'google', {
+    successRedirect: '/user',
+    failureRedirect: '/auth/google/failure'
+  })
+);
+
+router.get('/google', passport.authenticate('google', 
+{ scope: [ 'email', 'profile' ] }
+));
+
+router.get('/auth/google/failure', (req, res) => {
+    res.send('Failed to authenticate..');
+});
+
+module.exports = router;
--- src/controllers/login-logout/loginUser.js
+++ src/controllers/login-logout/loginUser.js
@@ -3,19 +3,23 @@ const passport = require('passport');
 const bcrypt = require('bcrypt')
 const LocalStrategy = require('passport-local');
 
-const { Users } = require('../db')
+const { Users } = require('../../db');
+
+const loginGoogle = require('./loginGoogle');
 
 const router = Router();
 
-passport.use(new LocalStrategy( async function verify(username, password, cb){
-    const user = await Users.findOne({where:{email: username}});
+router.use('/auth', loginGoogle);
+
+passport.use(new LocalStrategy( async function verify(email, password, cb){
+    const user = await Users.findOne({where:{email}});
     if(user && !user.isBanned){
         const passwordMatch = await bcrypt.compare(password, user.password)
-        if(username === user.email && passwordMatch){
-            return cb(null, {id:user.id, email:user.email})
+        if(email === user.email && passwordMatch){
+            return cb(null, {id:user.id, email:user.email});
         }
     }
-    return cb(null, false)
+    return cb(null, false);
 }));
 
 passport.serializeUser((user, done) =>{
@@ -34,5 +38,6 @@ router.post('/', passport.authenticate('local', {
 
 router.get('/', (req, res) =>{
     res.json({"message":'send a post'})
-})
+});
+
 module.exports = router;

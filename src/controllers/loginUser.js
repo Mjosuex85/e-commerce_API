45258dcf96108b7999dc11ca39420ea8b718ac30
aalred <aalex.red1@gmail.com>
2022-09-01 00:15:55 -0500

    added route for login

--- src/controllers/loginUser.js
+++ src/controllers/loginUser.js
@@ -1,12 +1,41 @@
 const { Router } = require('express');
+const passport = require('passport');
+const LocalStrategy = require('passport-local');
+
+const { Users } = require('../db')
+
 const router = Router();
 
-router.get('/', (req, res)=>{
-    res.send('ok')
-})
+const { KEY_SALT } = process.env;
+const keySalt = parseInt(KEY_SALT);
 
-router.post('/', (req, res)=>{
-    const {email, password} = req.body
-    res.send('ok')
-})
+
+passport.use(new LocalStrategy( async function verify(username, password, cb){
+    const user = await Users.findOne({where:{email: username}})
+    const passwordKey = await Users.findOne({where:{email: username}})
+    let passwordMatch;
+    bcrypt.hash(password, keySalt, async function (err, hash) {
+        if(err) throw new Error('something is wrong with the password at Login') 
+        passwordMatch = passwordKey === hash ? true : false;
+    })
+    if(username === user.email && passwordMatch){
+        return cb(null, {id:user.id, name:user.name})
+    }
+    return cb(null, false)
+}));
+
+passport.serializeUser((user, done) =>{
+    done(null, user.id)
+});
+
+passport.deserializeUser(async (id, done) => {
+    const user = await Users.findByPk(id) 
+    done(null, user)
+});
+
+router.post('/', passport.authenticate('local', {
+    successRedirect: '/platforms',
+    failureRedirect: '/signup'
+}));
+
 module.exports = router;

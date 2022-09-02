21348a1ff0aa66fc50785c375074515640de6760
linker <diegolucas142@gmail.com>
2022-09-02 00:51:56 -0300

    Feature/Route-UserRouter

--- src/controllers/UserRouter.js
+++ src/controllers/UserRouter.js
@@ -4,21 +4,65 @@ const router = Router();
 
 //---- edit user
 
-router.get("/", async(req,res)=>{  //si es por params no puedo hacer que retorne todo.
-    const {id} = req.query;
+router.get("/all", async (req,res)=>{ //anda
+    let data = await Users.findAll();
+    if(data.length){
+        res.status(200).send(data)
+    }else{
+        res.status(401).send("No users found");
+    };
+})
+
+router.get("/:id", async(req,res)=>{   //anda, si no se le pasa id se rompe
+    const {id} = req.params;
     //console.log(id)
-    try{
-        let data;
-        if(id){
-            data = await Users.findOne( {where: {id : id}});
-        }else{
-            console.log("entre al else")
-            data=await Users.findAll();
+        try{
+            let data;
+            if(id){
+                data = await Users.findOne( {where: {id : id}});
+                if(!data) return res.send("user not found")
+            }
+            res.status(200).send(data);
+        }catch(e){
+            console.error(e)
+            res.status(401).send("error al cargar los usuarios");
         };
-        res.status(200).send(data);
+});
+
+router.put("/ban/:id", async (req,res)=>{ //se rompe si no pasa id,
+    const {id} = req.params;
+    console.log(id)
+        try{
+            let user = await Users.findOne({where: {id: id}});
+            if (!user) return res.status(401).send("User not found")
+            if(user.isBanned === false){
+                user.isBanned = true
+            }/* else{
+                user.isBanned = false;
+            }; */
+            await user.save();
+            res.status(200).send(user);
+        }catch(e){
+            console.error(e);
+            res.status(401).send(e);
+        };
+});
+
+
+router.put("/unban/:id", async (req,res)=>{ //se rompe si no pasa id
+    const {id} = req.params;
+    let user = await Users.findOne({where: {id: id}});
+    if (!user) return res.status(401).send("User not found");
+    try{
+        if(user.isBanned === true){
+            user.isBanned = false;
+            console.log("user unbanned!")
+        }
+        await user.save();
+        res.status(200).send(user);
     }catch(e){
-        console.error(e)
-        res.status(401).send("error al cargar los usuarios");
+        console.error(e);
+        res.status(401).send(e);
     };
 });
 

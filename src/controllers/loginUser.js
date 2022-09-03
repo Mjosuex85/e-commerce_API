dc42c12369404ebba80e5d95b8523592fd988b85
aalred <aalex.red1@gmail.com>
2022-09-02 21:46:48 -0500

    restore files

--- src/controllers/loginUser.js
+++ src/controllers/loginUser.js
@@ -3,7 +3,7 @@ const passport = require('passport');
 const bcrypt = require('bcrypt')
 const LocalStrategy = require('passport-local');
 
-const { Users } = require('../../db');
+const { Users } = require('../db');
 
 const loginGoogle = require('./loginGoogle');
 

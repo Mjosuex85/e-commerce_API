9fba6c8f11012110fd8bdff1ec3c2e9a48b03ee1
aalred <aalex.red1@gmail.com>
2022-09-05 19:20:13 -0500

    fixed bugs files

--- src/controllers/landingUser.js
+++ src/controllers/landingUser.js
@@ -3,72 +3,71 @@ const { Router } = require('express');
 const { Users, AuthUsers } = require('../db');
 const router = Router();
 
-function isAuthenticated(req, res, next){
-    if(req.isAuthenticated()) return next();
+function isAuthenticated(req, res, next) {
+    if (req.isAuthenticated()) return next();
     res.redirect('/login');
 }
 
-router.get('/', isAuthenticated, async(req, res)=>{
+router.get('/', isAuthenticated, async (req, res) => {
     try {
-        const {id} = req.user.dataValues;
-
-        const user = await Users.findOne({ where: {id}, include:'Products'});
-        res.json({message: 'Welcome '+ user.username, user: user});
+        const { id } = req.user.dataValues;
+        const user = id.length > 3 ? await AuthUsers.findOne({ where: { id }, include: 'Products' }) : await Users.findOne({ where: { id }, include: 'Products' });
+        res.json({ message: 'Welcome ' + user.username, user: user });
     } catch (error) {
-        res.status(404).json({error: error.message});
+        res.status(404).json({ error: error.message });
     }
 })
 
-router.get('/addFavorite/:idProduct', isAuthenticated, async(req, res)=>{
+router.get('/addFavorite/:idProduct', isAuthenticated, async (req, res) => {
     try {
-        const {id} = req.user.dataValues;
-        const { idProduct }= req.params;
-        const user = typeof id === 'string'? await AuthUsers.findByPk(id) : await Users.findByPk(id);
-        user.addProducts(idProduct, {through: 'Favorites'});
+        const { id } = req.user.dataValues;
+        const { idProduct } = req.params;
+        const user = typeof id === 'string' ? await AuthUsers.findByPk(id) : await Users.findByPk(id);
+        user.addProducts(idProduct, { through: 'Favorites' });
         res.send('Added to Favorites');
     } catch (error) {
-        res.status(404).json({error: error.message});
+        res.status(404).json({ error: error.message });
     }
 })
 
-router.get('/buy/:idProduct', isAuthenticated, async(req, res)=>{
+router.get('/buy/:idProduct', isAuthenticated, async (req, res) => {
     try {
-        const {id} = req.user.dataValues;
-        const { idProduct }= req.params;
-        const user = typeof id === 'string'? await AuthUsers.findByPk(id) : await Users.findByPk(id);
-        user.addProducts(idProduct, {through: 'Order'});
+        const { id } = req.user.dataValues;
+        const { idProduct } = req.params;
+        const user = typeof id === 'string' ? await AuthUsers.findByPk(id) : await Users.findByPk(id);
+        user.addProducts(idProduct, { through: 'Order' });
         res.send('Buy succesfully');
     } catch (error) {
-        res.status(404).json({error: error.message});
+        res.status(404).json({ error: error.message });
     }
 });
 
-router.get('/find/email/:email', async(req, res)=>{
+router.get('/find/email/:email', async (req, res) => {
     try {
-        const {email} = req.params;
+        const { email } = req.params;
         const response = await Users.findOne({ where: { email } });
-        if(response){
-            res.json({'user': true})
-        }else{
-            res.json({'user': false})
+        if (response) {
+            res.json({ 'user': true })
+        } else {
+            res.json({ 'user': false })
         }
     } catch (error) {
         console.log(error)
-        res.status(404).json({error: error.message});
+        res.status(404).json({ error: error.message });
     }
 });
 
-router.get('/find/username/:username', async(req, res)=>{
+router.get('/find/username/:username', async (req, res) => {
     try {
-        const {username} = req.params;
-        const response = await Users.findOne({where:{username}});
-        if(response){
-            res.json({'user': true})
-        }else{
-            res.json({'user': false})
+        const { username } = req.params;
+        const response = await Users.findOne({ where: { username } });
+        if (response) {
+            res.json({ 'user': true })
+        } else {
+            res.json({ 'user': false })
         }
     } catch (error) {
-        res.status(404).json({error: error.message});
+        res.status(404).json({ error: error.message });
     }
 });
 module.exports = router;
--- src/controllers/loginUser.js
+++ src/controllers/loginUser.js
@@ -11,40 +11,25 @@ const router = Router();
 
 router.use('/auth', loginGoogle);
 
-passport.use(new LocalStrategy( async function verify(email, password, cb){
-    const user = await Users.findOne({where:{email}});
-    console.log(user)
-    if(user && !user.isBanned){
-    let passwordMatch = await bcrypt.compare(password, user.dataValues.password)
-    console.log(passwordMatch);
-    console.log(email === user.email);
-    if(email === user.email && passwordMatch){
-        console.log('--------------entro-----------------')
-        return cb(null, {id:user.id, email:user.email});
-    }}
+passport.use(new LocalStrategy(async function verify(email, password, cb) {
+    const user = await Users.findOne({ where: { email } });
+    if (user && !user.isBanned) {
+        let passwordMatch = await bcrypt.compare(password, user.password)
+        if (email === user.email && passwordMatch) {
+            return cb(null, { id: user.id, email: user.email });
+        }
+    }
     return cb(null, false);
 }));
 
-passport.serializeUser((user, done) =>{
-    console.log('serializeUser user')
-    done(null, user.id)
-});
-
-passport.deserializeUser(async (id, done) => {
-    console.log('deserializeUser user')
-    const user = await Users.findByPk(id) 
-    console.log('serializeUser: ' + user)
-    done(null, user)
-});
-
 router.post('/', passport.authenticate('local', {
-    successRedirect: "/user" ,
+    successRedirect: "/user",
     failureRedirect: "/login"
 }));
 
-router.get('/', (req, res) =>{
+router.get('/', (req, res) => {
     console.log('Not Autheticaded')
-    res.json({"message":'Not Autheticaded'})
+    res.json({ "message": 'Not Autheticaded' })
 });
 
 module.exports = router;

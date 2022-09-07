eecfc1593ab406d9e2f987320d53adebcbf7cd6e
aalred <99151344+aalred@users.noreply.github.com>
2022-09-07 13:18:25 -0500

    Update app.js

--- src/app.js
+++ src/app.js
@@ -20,9 +20,7 @@ server.use(bodyParser.json({ limit: '50mb' }));
 server.use(cookieParser(KEY_SECRET));
 server.use(morgan('dev'));
 server.use((req, res, next) => {
-console.log(req.headers.origin)
-  
-  res.header('Access-Control-Allow-Origin', 'https://e-commerce-videogames.vercel.app/');
+  res.header('Access-Control-Allow-Origin', 'https://e-commerce-videogames.vercel.app');
   res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
   res.header('Access-Control-Allow-Credentials', 'true');
   res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');

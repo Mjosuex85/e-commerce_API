75ce7a126044f985bad09213bfd868616e9f2a11
aalred <99151344+aalred@users.noreply.github.com>
2022-09-07 12:34:12 -0500

    Update app.js

--- src/app.js
+++ src/app.js
@@ -22,19 +22,11 @@ server.use(morgan('dev'));
 server.use((req, res, next) => {
 console.log(req.headers.origin)
   const whiteList = [
-    `${LOCALHOST1}`,
-    `${LOCALHOST2}`,
-    `${LOCALHOST3}`,
-    `${LOCALHOST4}`,
-    `${LOCALHOST5}`,
-    `${LOCALHOST6}`,
-    `${LOCALHOST7}`,
+    '*',
     'https://e-commerce-videogames.vercel.app',
   ]
-  if (whiteList.indexOf(req.headers.origin) !== -1) {
-    res.header('Access-Control-Allow-Origin', req.headers.origin);
+    res.header('Access-Control-Allow-Origin', 'https://e-commerce-videogames.vercel.app');
     res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
-  }
   res.header('Access-Control-Allow-Credentials', 'true');
   res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
   res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE');

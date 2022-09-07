edb7514d79f33a044dc4bac331750b2b6b065357
mrchurches <laureanoiglesias34@gmail.com>
2022-09-06 22:48:58 -0300

    fix

--- src/controllers/payment.js
+++ src/controllers/payment.js
@@ -5,7 +5,7 @@ const { ACCESS_TOKEN } = process.env;
 
 // Agrega credenciales
 mercadopago.configure({
-  access_token: process.env.ACCESS_TOKEN,
+  access_token: ACCESS_TOKEN,
 });
 
 let preference = {
@@ -15,7 +15,7 @@ let preference = {
         unit_price: 100,
         quantity: 1,
       },
-    ],
+    ]
   };
   
   
@@ -38,34 +38,3 @@ let preference = {
 });
 
 module.exports = router;
-
-// const mercadopago = require ('mercadopago');
-// const express = require('express');
-// const port = 3000;
-
-// mercadopago.configure({
-//   access_token: 'PROD_ACCESS_TOKEN'
-// });
-
-// app.post('/api/orders', (req, res) => {
-//   /* aquí crea tu orden en la DB para el usuario logeado */
-//   const order = db.orders.create({ userId: req.userId, productId: req.body.productId }); // <--- pseudo-código
-
-//   // Ahora le decimos a MP que cree la "preferencia". Asume que "order" tiene datos del producto
-//   mercadopago.preferences.create({
-//     items: [
-//       {
-//         title: order.product.name,
-//         unit_price: order.price,
-//         quantity: order.quantity,
-//       }
-//     ]
-//   }).then((preference) => {
-//     // el front recibirá el preferenceId :)
-//     res.json({ preferenceId: preference.id });
-//   });
-// });
-
-// app.listen(port, () => {
-//   console.log(`Example app listening at http://localhost:${port}`)
-// })

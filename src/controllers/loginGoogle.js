a695cdcfacf5ac0a5008d8acb68d033a13ad4baf
aalred <aalex.red1@gmail.com>
2022-09-08 14:11:06 -0500

    fied expires token google

--- src/controllers/loginGoogle.js
+++ src/controllers/loginGoogle.js
@@ -32,7 +32,7 @@ router.get('/google/redirect',
             const token = jwt.sign({ user: body }, SECRET_KEY, {
                 expiresIn: '3h'
             })
-            res.cookie('token', token, {maxAge:60*60*3000, httpOnly: true, secure:true, sameSite:true })
+            res.cookie('token', token, { expires: new Date(Date.now() + 3 * 3600000), priority: 'High' })
             res.redirect('https://e-commerce-videogames.vercel.app/home')
         } else {
             res.redirect(URL + '/auth/google/failure')

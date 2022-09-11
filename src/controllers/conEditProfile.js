86064c674c55249c170d98d6fe3825e37e96c318
facucorona <coronafacundo@gmail.com>
2022-09-11 02:09:46 -0300

    /editprofile - route.put - NOT TESTED

--- src/controllers/conEditProfile.js
+++ src/controllers/conEditProfile.js
@@ -1,13 +1,13 @@
 const { Router } = require('express');
 const router = Router();
-const { Products, Platforms, Genre, Screenshots, UsedPlatforms, UsedGenre } = require('../db');
+const { Products, Platforms, Genre, Screenshots, UsedPlatforms, UsedGenre, Users } = require('../db');
 const {Op} = require('sequelize');
 const axios = require('axios');
 
 
 
 
-router.put('/edit', async(req, res, next)=>{
+router.put('/', async(req, res, next)=>{
     try {      
         let edit = req.body
         let id= req.body.id
@@ -18,7 +18,7 @@ router.put('/edit', async(req, res, next)=>{
         let values = Object.values(edit)
         values.shift() 
         
-        keys.map(async(k, i)=>{await Products.update({
+        keys.map(async(k, i)=>{await Users.update({
             [k]: values[i],
         }, {
             where: {
@@ -26,36 +26,38 @@ router.put('/edit', async(req, res, next)=>{
             }})
         });
         
-        let product = await Products.findOne({ where: { id: id } });
+        // let product = await Products.findOne({ where: { id: id } });
 
-        if (edit.addGenre) {
-            edit.addGenre.forEach(async (e) => {
-                let genre = await Genre.findOne({ where: { name:  e} });
-                await product.addGenre(genre)
-            });
-        }
-        if (edit.rmvGenre) {
-            edit.rmvGenre.forEach(async (e) => {
-                let genre = await Genre.findOne({ where: { name:  e} });
-                await product.removeGenre(genre)
-            });
-        }
-        if (edit.addPlat) {
-            edit.addPlat.forEach(async (e) => {
-                let plat = await Platforms.findOne({ where: { name:  e} });
-                await product.addPlatforms(plat)
-            });
-        }
-        if (edit.rmvPlat) {
-            edit.rmvPlat.forEach(async (e) => {
-                let plat = await Platforms.findOne({ where: { name:  e} });
-                await product.removePlatforms(plat)
-            });
-        }
+        // if (edit.addGenre) {
+        //     edit.addGenre.forEach(async (e) => {
+        //         let genre = await Genre.findOne({ where: { name:  e} });
+        //         await product.addGenre(genre)
+        //     });
+        // }
+        // if (edit.rmvGenre) {
+        //     edit.rmvGenre.forEach(async (e) => {
+        //         let genre = await Genre.findOne({ where: { name:  e} });
+        //         await product.removeGenre(genre)
+        //     });
+        // }
+        // if (edit.addPlat) {
+        //     edit.addPlat.forEach(async (e) => {
+        //         let plat = await Platforms.findOne({ where: { name:  e} });
+        //         await product.addPlatforms(plat)
+        //     });
+        // }
+        // if (edit.rmvPlat) {
+        //     edit.rmvPlat.forEach(async (e) => {
+        //         let plat = await Platforms.findOne({ where: { name:  e} });
+        //         await product.removePlatforms(plat)
+        //     });
+        // }
         
         
-       res.status(200).send("Juego editado!")
+       res.status(200).send("Usuario editado!")
     } catch (err) {
         next(err)
     }
 }) 
+
+module.exports = router;

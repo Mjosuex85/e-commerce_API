1048414e9f931a366a6df8d220289024c09bc161
genti91 <franco.g7@hotmail.com>
2022-09-15 00:38:08 -0300

    paypal

---
+++ src/controllers/paypal.js
@@ -0,0 +1,103 @@
+const paypal = require("paypal-rest-sdk");
+const { Router } = require('express');
+const router = Router();
+
+paypal.configure({
+    mode: "sandbox", //sandbox or live
+    client_id:
+        "AX6SYjfkRQUQvmPBwEsfB8dcMjOZ6IoccjHqPbxmXWIHN5m-W38Ed4KWYANgEpenI2fISiF6JDW880dw",
+    client_secret:
+        "EMzJANetjGL5GEyh2NqZSor1WEXMEgJ_tKIxls6BkMEqSvKw55qz5pY6rsJIlhg3OV6bu2dtyya18pLf"
+});
+
+router.get("/rend", (req, res) => {
+    res.render("index");
+});
+
+router.get("/", (req, res) => {
+    try{
+    var create_payment_json = {
+        intent: "sale",
+        payer: {
+            payment_method: "paypal"
+        },
+        redirect_urls: {
+            return_url: "http://192.168.0.98:3001/paypal/success",
+            cancel_url: "http://192.168.0.98:3001/paypal/cancel"
+        },
+        transactions: [
+            {
+                item_list: {
+                    items: [
+                        {
+                            name: "item",
+                            sku: "item",
+                            price: "0.01",
+                            currency: "BRL",
+                            quantity: 1
+                        }
+                    ]
+                },
+                amount: {
+                    currency: "BRL",
+                    total: "0.01"
+                },
+                description: "This is the payment description."
+            }
+        ]
+    };
+
+    paypal.payment.create(create_payment_json, function(error, payment) {
+        if (error) {
+            throw error;
+        } else {
+            console.log("Create Payment Response");
+            console.log(payment);
+            res.redirect(payment.links[1].href);
+        }
+    });
+    }catch(err){
+        console.log(err)
+    }
+});
+
+router.get("/success", (req, res) => {
+    try{
+    var PayerID = req.query.PayerID;
+    var paymentId = req.query.paymentId;
+    var execute_payment_json = {
+        payer_id: PayerID,
+        transactions: [
+            {
+                amount: {
+                    currency: "BRL",
+                    total: "0.01"
+                }
+            }
+        ]
+    };
+
+    paypal.payment.execute(paymentId, execute_payment_json, function(
+        error,
+        payment
+    ) {
+        if (error) {
+            console.log(error.response);
+            throw error;
+        } else {
+            console.log("Get Payment Response");
+            console.log(JSON.stringify(payment));
+            res.render("success");
+        }
+    });
+    }catch(err){
+        console.log(err)
+    }
+});
+
+router.get("cancel", (req, res) => {
+    res.render("cancel");
+});
+
+
+module.exports = router;

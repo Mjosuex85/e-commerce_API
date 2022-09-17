316231598cf0843840d20a45b9f18abec748990e
facucorona <coronafacundo@gmail.com>
2022-09-17 02:36:33 -0300

    restore pass funcionando - falta diseñar mail y mostrar en pantalla respuestas

--- src/controllers/conRestore.js
+++ src/controllers/conRestore.js
@@ -17,70 +17,67 @@ router.put('/restore', async (req, res, next)=>{
     }
 });
 
-router.get('/restore', async (req, res, next)=>{
+router.get('/', async (req, res, next)=>{
     try{
-        const {email}= req.body;
+        const {email}= req.query;
+        console.log("🚀 ~ file: conRestore.js ~ line 23 ~ router.get ~ email", email)
+        
         if (!email){
             return res.status(400).json({message:"Insert a registered E-Mail please"});
         }
         const message=`Please check your e-mail for your link to change the password.`;
         let verificationLink;
         let emailStatus = 'Ok';
-        // let userGoogle = AuthUser.findAll()
+        
 
         // try{ -- AGREGADO
-        let userLocal = await Users.findOneOrFail({where:{email: email}})
-        // let allUsers = userLocal.concat(userGoogle)
-        // let searchResults = allUsers.filter(u=>e.email===email)
+        let userLocal = await Users.findOne({where:{email: email}})
         
-        const token = jwt.sign({userId: userLocal.id, username: userLocal.username}, SECRET_KEY, { expiresIn: '5m'})
-        verificationLink= `${URL}restore/newpassword/${userLocal.id}/${token}`
-        email.resetToken=token
+        const token = jwt.sign({userId: userLocal.id, username: userLocal.username}, SECRET_KEY, { expiresIn: '45m'})
+
+        verificationLink= `${process.env.URL_ALLOWED}/changepass/${userLocal.id}/${token}`
+        
+        userLocal.resetToken=token        
+
+        res.send(userLocal.resetToken)
+        forgotPassword(email, verificationLink);
         // }catch (error){ -- AGREGADO
         //     return res.json({"El usuario ingresado no esta registrado en la base de datos"})
         // }
 
-
-        if (searchResults[0].d.length > 6){
-            AuthUser.save(searchResults[0])
-
-        }
         
     }catch(e){
         next(e)
     }
 });
 
-router.put('/restore/newpassword/:id/:token', async (req, res, next)=>{
+router.put('/newpassword/:id/:token', async (req, res, next)=>{
     try{
         const { newPassword } = req.body;
-        const resetToken = req.headers.reset
-        resetToken = JSON.stringify(resetToken)
+       
+        const { token } = req.params;
+        const { id } = req.params;
+        
+        
+        resetToken = JSON.stringify(token)
 
-        if ( !newPassword || !resetToken){
+        if ( !newPassword || !token){
             res.status(400).json({message: "All fields are required "})
         }
-        let jwtPayload = jwt.verify(resetToken, SECRET_KEY);
-        let user = await Users.findOneOrFail({where:{resetToken}})
-
-        user.password = newPassword;
-        const validationOps = {validationError:{target:false, value:false}};
-        const errors = await validate(user, validationOps);
-
-        // if(errors.length > 0){ -- AGREGADO
-        //    return res.status(401).json(errors);
-        // }
-
-
-        // try{
-
-        user.hashPassword()
-        await  Users.save(user);
-        // } catch (error){ -- AGREGADO
-        //     return res.status(400).json({message: "Something goes wrong"})
-        // }
-        // res.json({message: "Password chenged!"})
-
+        let jwtPayload = jwt.verify(token, SECRET_KEY);
+        console.log("🚀 ~ file: conRestore.js ~ line 75 ~ router.put ~ jwtPayload", jwtPayload)
+        
+       
+        await Users.update({
+                password: newPassword,
+            }, {
+                where: {
+                    id: [id],
+            }})
+        
+        res.status(200).send({message:"Password Changed!"})
+        
+       
         //falta res, ver video min24'
 
     }catch(e){
